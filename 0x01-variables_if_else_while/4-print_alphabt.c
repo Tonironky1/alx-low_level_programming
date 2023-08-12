244479a2cff3674d7e372e8c198eaaabf2fb5b96
Tonironky1 <tonironky@yahoo.com>
2023-08-11 20:13:45 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,21 +9,19 @@
 
 
 int main(void)
-        
+
 {
-        
-       char low, e, q;
-        
-        e = 'e';
-        q = 'q';
-        
-        for (low = 'a'; low <= 'z'; low++)
+
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
 	{
 	if (low != e && low != q)
 	putchar(low);
 	}
 	putchar('\n');
-        return (0);
-
+	return (0);
 }
-               
